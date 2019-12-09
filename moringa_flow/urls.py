75605c5c65954c601617598856323f7bdf9f7f12
Jay-68 <kelvinngari1995@gmail.com>
2019-12-09 13:01:28 +0300

    Add application urls to the main project

--- moringa_flow/urls.py
+++ moringa_flow/urls.py
@@ -14,8 +14,22 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.views.generic import TemplateView
+from django.conf import settings
+from django.conf.urls.static import static
+from groups import views
+from questions import views as v
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
+	path('accounts/', include('accounts.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
+	path('about/',TemplateView.as_view(template_name="about.html"), name="about"),
+	path('',views.GroupList.as_view(),name='home'),
+	path('groups/', include('groups.urls')),
+	path('question/', include('questions.urls')),
+	path('create/question/', v.CreateQuestion.as_view(), name='create_question'),
+
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
