72d76b04bbe6e5713b1a84faae0c141daf8b5fe5
Jay-68 <kelvinngari1995@gmail.com>
2019-12-09 12:36:28 +0300

    New start

---
+++ env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/moringa/James/Moringa_School_projects/moringa_flow/env/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
