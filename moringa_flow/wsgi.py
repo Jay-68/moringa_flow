e2a926aebe617621d4aa2f94a6cb2d12baf0edd4
Jay-68 <kelvinngari1995@gmail.com>
2019-12-09 15:38:27 +0300

    create urls for redirecting to the main page after login

--- moringa_flow/wsgi.py
+++ moringa_flow/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'quora_clone_project.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'moringa_flow.settings')
 
 application = get_wsgi_application()
